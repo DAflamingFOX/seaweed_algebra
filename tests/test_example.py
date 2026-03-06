e58c1abc69a0964ec5706e228074d4ad943be698
Jeffrey Morris <ffejcsirrom@gmail.com>
2026-03-06 15:26:54 -0600

    Update Test to resolve lint error.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,9 @@
-import seaweed_algebra
+from seaweed_algebra import example
 
 def test_pipeline_works():
     """A simple dummy test to ensure the CI pipeline runs successfully."""
     assert True
+
+def test_import():
+    """Test importing something from the library. Check that 1+1=2, literally."""
+    assert example.add_one(1) == 2
