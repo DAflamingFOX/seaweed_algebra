5c1658d90dda5a8147b4229d27c0d85eaccf685b
Jeffrey Morris <ffejcsirrom@gmail.com>
2026-03-12 18:01:18 -0500

    Update latex_utils to support both axes and figure returning functions.

--- src/seaweed_algebra/latex_utils.py
+++ src/seaweed_algebra/latex_utils.py
@@ -2,10 +2,11 @@ from typing import Callable, Literal
 
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 
 def export_as_pgf(
-    plot_mpl: Callable[[], Axes],
+    plot_mpl: Callable[[], Figure | Axes],
     filename: str,
     texsystem: Literal["pdflatex", "lualatex", "xelatex"] = "pdflatex",
 ) -> None:
@@ -16,8 +17,9 @@ def export_as_pgf(
 
     with plt.rc_context(pgf_config):
         # Create a fresh figure using the supplied callable.
-        ax = plot_mpl()
-        fig = ax.get_figure()
+        x = plot_mpl()
+
+        fig = x if isinstance(x, Figure) else x.get_figure()
 
         fig.savefig(filename, bbox_inches="tight", transparent=True)
 
