0be035c71275fbee7b888f29eda4bf93d50eaa90
Jeffrey Morris <ffejcsirrom@gmail.com>
2026-03-19 19:22:31 -0500

    Update demos to match new files

--- demo/basic_plots.py
+++ demo/basic_plots.py
@@ -4,8 +4,7 @@ from matplotlib import pyplot as plt
 
 from seaweed_algebra import Meander, Seaweed
 from seaweed_algebra.latex_utils import export_as_pgf
-from seaweed_algebra.visual.meander_plot import MeanderPlot
-from seaweed_algebra.visual.seaweed_plot import SeaweedPlot
+from seaweed_algebra.visual import plot_meander, plot_seaweed
 
 # Create a few seaweeds and meanders.
 sw1 = Seaweed([4, 1], [2, 1, 2])
@@ -20,8 +19,8 @@ sw3 = Seaweed.from_meander(m3)
 # Plot the afformentioned seaweeds and meanders.
 f1, (f1_ax1, f1_ax2) = plt.subplots(1, 2, figsize=(7, 3), layout="constrained")
 
-MeanderPlot(m1).plot(f1_ax1)
-SeaweedPlot(sw1).plot(f1_ax2)
+plot_meander(m1, f1_ax1)
+plot_seaweed(sw1, f1_ax2)
 
 f1_ax1.set_title("Meander")
 f1_ax2.set_title("Seaweed")
@@ -30,9 +29,8 @@ f1.suptitle(r"Meander and Seaweed of type $\frac{4|1}{2|1|2}$.")
 
 def generate_fig2():
     f2, (f2_ax1, f2_ax2) = plt.subplots(1, 2, figsize=(7, 3), layout="constrained")
-    # Try scaling this one up, I'm not super happy with the astrisks yet, perhaps dots?
-    SeaweedPlot(sw2).plot(f2_ax2)
-    MeanderPlot(m2).plot(ax=f2_ax1)
+    plot_seaweed(sw2, f2_ax2, nonzero_element=".", draw_diagonal=False)
+    plot_meander(m2, ax=f2_ax1)
 
     f2_ax1.set_title("Meander")
     f2_ax2.set_title("Seaweed")
@@ -44,11 +42,10 @@ def generate_fig2():
 generate_fig2()
 
 # You don't need to provide a MPL Axes object...
-MeanderPlot(m3).plot(arc_scale=0.5)
+plot_meander(m3, arc_scale=0.5)
 
-export_as_pgf(SeaweedPlot(sw1).plot, "./graphs/seaweed1.pgf")
+export_as_pgf(partial(plot_seaweed, sw1), "./graphs/seaweed1.pgf")
 export_as_pgf(generate_fig2, "./graphs/fig2")
-export_as_pgf(partial(MeanderPlot(m3).plot, arc_scale=0.5), "./graphs/meander3")
-
+export_as_pgf(partial(plot_meander, m3, arc_scale=0.5), "./graphs/meander3")
 
 plt.show()
--- demo/signature_homotopy.py
+++ demo/signature_homotopy.py
@@ -9,25 +9,19 @@ m = Meander([17, 3], [10, 4, 6])
 moves = m.signature()
 
 # Calculate the Homotopy type, H(c_1, ..., c_h), this returns a list of c values.
-homotopy = m.homotopy_type()
+homotopy = m.homotopy()
 
 # Print our results.
-meander_name = (
-    f"{'|'.join(map(str, m.top_blocks))} by {'|'.join(map(str, m.bottom_blocks))}"
-)
 homotopy_type = f"H({','.join(map(str, homotopy))})"
-print(f"\nThe signature of {meander_name} is:\n{',\n'.join(m.name for m in moves)}")
-print(f"\nThe homotopy type of {meander_name} is: {homotopy_type}")
-
+print(f"\nThe signature of {m} is: {''.join(m.value for m in moves)}")
+print(f"\nThe homotopy type of {m} is: {homotopy_type}")
 
 # Display the plots
-input("Press Enter to display the plots...\n")
-
 _, ax_dict = plt.subplot_mosaic("AA.B", figsize=(10, 3))
 
 m.draw_matplotlib(ax=ax_dict["A"])
 ax_dict["A"].set_title(
-    rf"Meander of type $\frac{{{meander_name.split(' by ')[0]}}}{{{meander_name.split(' by ')[1]}}}$"
+    rf"Meander of type $\frac{{{str(m).split(' / ')[0]}}}{{{m.split(' / ')[1]}}}$"
 )
 
 m_homotopy = Meander(homotopy, homotopy)
